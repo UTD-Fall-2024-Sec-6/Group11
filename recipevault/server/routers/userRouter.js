81b05364203d98292013fa9eea5cef0f9f2b4f84
AryamanTomer <101023524+AryamanTomer@users.noreply.github.com>
2024-11-26 17:29:43 -0600

    Update userRouter.js

--- recipevault/server/routers/userRouter.js
+++ recipevault/server/routers/userRouter.js
@@ -6,7 +6,7 @@ import { isAuthenticated, isAdmin } from "../middleware/authMiddleware.js";
 const userRouter = express.Router();
 
 
-userRouter.get("/", isAuthenticated, isAdmin, async (req, res) => {
+userRouter.get("/", async (req, res) => {
     try {
         const users = await User.find().select("-Password");
         res.status(200).json(users);
